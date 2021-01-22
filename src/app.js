a6d24fd0d5d99b8dfc8d69af1ccc9b7145c95e3e
Yokesh <yokeshmanikandan@gmail.com>
2021-01-22 14:55:22 +0530

    setting up app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port =process.env.PORT || 3000
 
 //Define path for express config
 const publicFileDirPath = path.join(__dirname,'../public')
@@ -87,6 +88,6 @@ app.get('*' , (req,res)=>{
     })
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('Server started!')
 })
